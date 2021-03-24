47491f84066fe1e8e5c3fe481ca283a37181b703
aaanandan <anandan.bs@gmail.com>
2021-03-24 18:45:05 +0530

    Updates

---
+++ precache-manifest.e305c19d82ef1c452cfc03e3e9e49cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taglist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df787b6924e7790d8e72",
+    "url": "/taglist/static/js/runtime~main.32f64371.js"
+  },
+  {
+    "revision": "388c485d86ea445c647e",
+    "url": "/taglist/static/js/main.da16f4c9.chunk.js"
+  },
+  {
+    "revision": "b465470f5937735a9606",
+    "url": "/taglist/static/js/2.566693d2.chunk.js"
+  },
+  {
+    "revision": "388c485d86ea445c647e",
+    "url": "/taglist/static/css/main.949f93b8.chunk.css"
+  },
+  {
+    "revision": "b465470f5937735a9606",
+    "url": "/taglist/static/css/2.61ee8d74.chunk.css"
+  },
+  {
+    "revision": "47e56f6e96384e354b9d2b4ba22639b3",
+    "url": "/taglist/index.html"
+  }
+];
